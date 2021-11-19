bb622337ca44c2fcf4d36a1c15e6e9635dc1dd7c
SaideepReddyKalukurthi <saideepreddy.k2@gmail.com>
2021-11-19 14:48:48 +0530

    moving folder up

---
+++ service-account/main.tf
@@ -0,0 +1,13 @@
+
+#Service account
+module "service_accounts" {
+  source        = "terraform-google-modules/service-accounts/google"
+  version       = "~> 3.0"
+  project_id    = var.project_id
+  prefix        = "demo-saideepreddy"
+  names         = ["first"]
+  project_roles = [
+    "${var.project_id}=>roles/compute.networkAdmin",
+    "${var.project_id}=>roles/storage.admin",
+  ]
+}
---
+++ service-account/variables.tf
@@ -0,0 +1,13 @@
+variable "project_id" {
+    type = string
+    default = "terraform-practice-001"
+}
+variable "region" {
+    type = string
+    default = "asia-south1"
+}
+variable "zone" {
+    type = string
+    default = "asia-south1-a"
+}
+
